3e4203dc3151639b07488233d79e5a6505b2b6ef
Yuvasree-M <yuvasreemohan4sep@gmail.com>
2025-04-02 15:33:23 +0530

    Commit

---
+++ src/main/java/JenkinsDemo/JenkinnsDemo/App.java
@@ -0,0 +1,13 @@
+package JenkinsDemo.JenkinnsDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
